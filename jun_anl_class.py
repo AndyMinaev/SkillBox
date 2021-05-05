8b3d3b12206bb50cc046346f5afe6d49fab0d3d9
Andy Minaev <sir.forsage@gmail.com>
2021-05-05 13:10:46 +0300

    Работает умножение

--- jun_anl_class.py
+++ jun_anl_class.py
@@ -15,9 +15,9 @@ class Fraction:
         else:
             if isinstance(other, Fraction):
                 if denominator != other.denominator:
+                    # приводим дробь к единому знаменателю, правда не меньшему, лень заморачиваться :)
                     numerator = numerator * other.denominator + other.numerator * denominator
                     denominator *= other.denominator
-                # складываем числители и выводим итоговый результат
                 result = Fraction(numerator, denominator)
                 return result
             else:
@@ -33,6 +33,7 @@ class Fraction:
         else:
             if isinstance(other, Fraction):
                 if denominator != other.denominator:
+                    # приводим дробь к единому знаменателю, правда не меньшему, лень заморачиваться :)
                     numerator = numerator * other.denominator - other.numerator * denominator
                     denominator *= other.denominator
                 # складываем числители и выводим итоговый результат
@@ -43,12 +44,28 @@ class Fraction:
         result = Fraction(numerator, denominator)
         return result
 
-
+    def __mul__(self, other):
+        numerator = self.numerator
+        denominator = self.denominator
+        if isinstance(other, int):
+            numerator *= other
+        else:
+            if isinstance(other, Fraction):
+                numerator *= other.numerator
+                denominator *= other.denominator
+                result = Fraction(numerator, denominator)
+                return result
+            else:
+                return 'Ошибка ввода данных'
+        result = Fraction(numerator, denominator)
+        return result
 
 
 number_1 = Fraction(1, 2)
 number_2 = Fraction(1, 4)
-print(number_1 + number_2)
-print(number_1 + 2)
-print(number_1 - number_2)
-print(number_1 - 2)
+print('{} + {} = {}'.format(number_1, number_2, number_1 + number_2))
+print('{} + {} = {}'.format(number_1, 2, number_1 + 2))
+print('{} - {} = {}'.format(number_1, number_2, number_1 - number_2))
+print('{} - {} = {}'.format(number_1, 2, number_1 - 2))
+print('{} * {} = {}'.format(number_1, number_2, number_1 * number_2))
+print('{} * {} = {}'.format(number_1, 2, number_1 * 2))
