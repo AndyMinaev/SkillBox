d4e8cc981216a160fa3f635c2cabd7ccc819b1a8
Andy Minaev <sir.forsage@gmail.com>
2021-05-11 23:23:57 +0300

    added os_info.txt

--- test.py
+++ test.py
@@ -7,3 +7,6 @@ info = 'OS info is \n{}\n\nPython version is {} {}'.format(
     platform.architecture()
 )
 print(info)
+
+with open('os_info.txt', 'w', encoding='utf8') as file:
+    file.write(info)
